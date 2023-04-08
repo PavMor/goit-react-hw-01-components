4284152184a6c67612ef1f7ba127d02649bdb71b
Your Name <pavlo.morozenko@ukr.net>
2023-04-08 14:46:18 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
+import { Profile } from './Profile/Profile';
+
+import user from './Data/user.json';
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
-    <Statistics title="Upload stats" stats={data} />
-    <FriendList friends={friends} />;
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+
+    // <Statistics title="Upload stats" stats={data} />
+    // <FriendList friends={friends} />;
   );
 };

7b89485e39e3c5d6654c8e1081d04ea0dcc3bc3d
Your Name <pavlo.morozenko@ukr.net>
2023-04-09 10:55:52 +0200

    7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './Transactions/Transactions';
 
 import user from './Data/user.json';
 import data from './Data/data.json';
 import friends from './Data/friends.json';
+import transactions from './Data/transactions.json';
 export const App = () => {
   return (
     <div>
@@ -17,6 +19,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
